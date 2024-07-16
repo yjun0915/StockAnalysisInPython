9be1f217bf13b9192955d34634f641934e7587cc
yjun0915 <kyjun09150916@gmail.com>
2024-07-16 22:49:33 +0900

    feature read from url

--- Investar/DBUpdater.py
+++ Investar/DBUpdater.py
@@ -1,7 +1,8 @@
 # 05_StockPriceAPI\Investar\DBUpdater.py
 
 import pymysql
-import pandas
+import pandas as pd
+
 
 class DBUpdater:
     def __init__(self):
@@ -42,6 +43,13 @@ class DBUpdater:
 
     def read_krx_code(self):
         """KRX로부터 상장법인목록 파일을 읽어와서 데이터프레임으로 반환"""
+        url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method='\
+              'download&searchType=13'
+        krx = pd.read_html(url, header=0)[0]
+        krx = krx[['종목코드', '회사명']]
+        krx = krx.rename(columns={'종목코드': 'code', '회사명': 'company'})
+        krx.code = krx.code.map('{:06d}'.format)
+        return krx
 
     def update_comp_info(self):
         """종목코드를 company_info 테이블에 업데이트한 후 딕셔너리에 저장"""
@@ -57,6 +65,7 @@ class DBUpdater:
 
     def execute_daily(self):
         """실행 즉시 및 매일 오후 다섯시에 daily_price 테이블 업데이트"""
+
 
 if __name__ == '__main__':
     dbu = DBUpdater()
