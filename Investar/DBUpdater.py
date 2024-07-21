278f41efbf7f3a4acbe77363b2f176b57befb13b
yjun0915 <kyjun09150916@gmail.com>
2024-07-21 13:37:38 +0900

    feature write into database and update

--- Investar/DBUpdater.py
+++ Investar/DBUpdater.py
@@ -43,8 +43,7 @@ class DBUpdater:
 
     def read_krx_code(self):
         """KRX로부터 상장법인목록 파일을 읽어와서 데이터프레임으로 반환"""
-        url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method='\
-              'download&searchType=13'
+        url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
         krx = pd.read_html(url, header=0)[0]
         krx = krx[['종목코드', '회사명']]
         krx = krx.rename(columns={'종목코드': 'code', '회사명': 'company'})
@@ -55,6 +54,26 @@ class DBUpdater:
         """종목코드를 company_info 테이블에 업데이트한 후 딕셔너리에 저장"""
         sql = "SELECT * FROM company_info"
         df = pd.read_sql(sql, self.conn)
+        for idx in range(len(df)):
+            self.codes[df['code'].values[idx]]=df['company'].values[idx]
+        with self.conn.cursor() as curs:
+            sql = "SELECT max(last_update) FROM company_info"
+            curs.execute(sql)
+            rs = curs.fetchone()
+            today = datetime.today().strftime('%Y-%m-%d')
+
+            if rs[0] == None or rs[0].strftime('%Y-%m-%d') < today:
+                krx = self.read_krx_code()
+                for idx in range(len(krx)):
+                    code = krx.code.values[idx]
+                    company = krx.company.values[idx]
+                    sql = f"REPLACE INTO company_info (code, company, last_update) VALUES ('{code}', '{company}', '{today}')"
+                    curs.execute(sql)
+                    self.codes[code] = company
+                    tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
+                    print(f"[{tmnow}] {idx:04d} REPLACE INTO company_info VALUES ({code}, {company}, {today})")
+                self.conn.commit()
+                print('')
 
     def read_naver(self, code, company, pages_to_fetch):
         """네이버 금융에서 주식 시세를 읽어서 데이터프레임으로 반환"""
