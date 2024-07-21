1786ee10ace88be3dcb01fcc37828759d9f856c7
yjun0915 <kyjun09150916@gmail.com>
2024-07-21 15:52:31 +0900

    feature update db > daily_price

--- Investar/DBUpdater.py
+++ Investar/DBUpdater.py
@@ -99,10 +99,8 @@ class DBUpdater:
                 pg_url = '{}&page={}'.format(url, page)
                 df = df.append(pd.read_html(pg_url, header=0)[0])
                 tmnow = datetime.now().strftime("%Y-%m-%d %H:%M")
-                print('[{}] {} ({}) : {:04d}/{:04d} pages are downloading...',
-                      format(tmnow, company, code, page, pages), end="\r")
-            df = df.rename(columns={'날짜':'date','종가':'close','전일비':'diff'
-                    ,'시가':'open','고가':'high','저가':'low','거래량':'volume'})
+                print('[{}] {} ({}) : {:04d}/{:04d} pages are downloading...'.format(tmnow, company, code, page, pages), end="\r")
+            df = df.rename(columns={'날짜': 'date', '종가': 'close', '전일비': 'diff', '시가': 'open', '고가': 'high', '저가': 'low', '거래량': 'volume'})
             df['date'] = df['date'].replace('.', '-')
             df = df.dropna()
             df[['close', 'diff', 'open', 'high', 'low', 'volume']] = df[['close','diff', 'open', 'high', 'low', 'volume']].astype(int)
@@ -112,9 +110,14 @@ class DBUpdater:
             return None
         return df
 
-
     def replace_into_db(self, df, num, code, company):
         """네이버 금융에서 읽어온 주식 시세를 DB에 REPLACE"""
+        with self.conn.cursor() as curs:
+            for r in df.itertuples():
+                sql = f"REPLACE INTO daily_price VALUES ('{code}', '{r.date}', '{r.open}', '{r.high}', '{r.low}', '{r.close}', '{r.diff}', '{r.volume}')"
+                curs.execute(sql)
+            self.conn.commit()
+            print('[{}] #{:04d} {} ({}) : {} rows > REPLACE INTO daily_price [OK]'.format(datetime.now().strftime('%Y-%m-%d %H:%M'), num+1, company, code, len(df)))
 
     def update_daily_price(self, pages_to_fetch):
         """KRX 상장법인의 주식 시세를 네이버로부터 읽어서 DB에 업로드"""
