4b12997e90a117ae959c77a6862ed08c34c0270d
yjun0915 <kyjun09150916@gmail.com>
2024-07-24 17:59:29 +0900

    feature routing document MainPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <>hello</>;
-}
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./Pages/Mainpage";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
---
+++ frontend/src/Pages/Mainpage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainPage: React.FC = () => {
+  return <>this is a mainpage</>;
+};
+
+export default MainPage;
